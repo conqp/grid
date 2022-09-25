9021fcee525861ac53382e13a50c42aee8e2c14f
Richard Neumann <mail@richard-neumann.de>
2022-09-25 11:43:22 +0200

    Refactor to using one flat vector

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,10 @@
 pub struct Grid<T> {
     width: usize,
     height: usize,
-    items: Vec<Vec<T>>,
+    items: Vec<T>,
 }
+
+const NEIGHBOR_OFFSETS: [isize; 3] = [-1, 0, 1];
 
 impl<T> Grid<T> {
     pub fn new(width: usize, height: usize, initializer: impl Fn() -> T) -> Self {
@@ -11,7 +13,7 @@ impl<T> Grid<T> {
             width,
             height,
             items: (0..height)
-                .map(move |_| (0..width).map(|_| initializer()).collect())
+                .flat_map(|_| (0..width).map(|_| initializer()))
                 .collect(),
         }
     }
@@ -30,7 +32,7 @@ impl<T> Grid<T> {
 
     pub fn get(&self, x: usize, y: usize) -> Option<&T> {
         if x < self.width() && y < self.height() {
-            Some(&self.items[y][x])
+            Some(&self.items[self.coordinate_to_index(x, y)])
         } else {
             None
         }
@@ -38,60 +40,57 @@ impl<T> Grid<T> {
 
     pub fn get_mut(&mut self, x: usize, y: usize) -> Option<&mut T> {
         if x < self.width() && y < self.height() {
-            Some(&mut self.items[y][x])
+            let index = self.coordinate_to_index(x, y);
+            Some(&mut self.items[index])
         } else {
             None
         }
     }
 
     pub fn iter(&self) -> impl Iterator<Item = &T> {
-        self.items.iter().flat_map(|row| row)
+        self.items.iter()
     }
 
     pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut T> {
-        self.items.iter_mut().flat_map(|row| row)
+        self.items.iter_mut()
     }
 
     pub fn enumerate(&self) -> impl Iterator<Item = (usize, usize, &T)> {
-        self.items
-            .iter()
-            .enumerate()
-            .flat_map(|(y, row)| row.iter().enumerate().map(move |(x, item)| (x, y, item)))
-    }
-
-    pub fn enumerate_mut(&mut self) -> impl Iterator<Item = (usize, usize, &mut T)> {
-        self.items.iter_mut().enumerate().flat_map(|(y, row)| {
-            row.iter_mut()
-                .enumerate()
-                .map(move |(x, item)| (x, y, item))
+        self.items.iter().enumerate().map(|(idx, item)| {
+            let (x, y) = self.index_to_coordinate(idx);
+            (x, y, item)
         })
     }
 
     pub fn neighbors(&self, x: usize, y: usize) -> impl Iterator<Item = (usize, usize, &T)> {
-        self.enumerate().filter(move |(other_x, other_y, _)| {
-            is_neighbor(other_x.abs_diff(x), other_y.abs_diff(y))
-        })
+        self.neighbor_indices(x, y)
+            .map(|(x, y)| (x, y, self.get(x, y).unwrap()))
     }
 
-    pub fn neighbors_mut(
-        &mut self,
-        x: usize,
-        y: usize,
-    ) -> impl Iterator<Item = (usize, usize, &mut T)> {
-        self.enumerate_mut().filter(move |(other_x, other_y, _)| {
-            is_neighbor(other_x.abs_diff(x), other_y.abs_diff(y))
-        })
+    fn neighbor_indices(&self, x: usize, y: usize) -> impl Iterator<Item = (usize, usize)> + '_ {
+        neighbor_offsets()
+            .map(move |(dx, dy)| (x as isize + dx, y as isize + dy))
+            .filter(|(dx, dy)| {
+                // remove elements outside of grid
+                0 <= *dx && *dx < self.width as isize && 0 <= *dy && *dy < self.height as isize
+            })
+            .map(|(dx, dy)| (dx as usize, dy as usize))
+    }
+
+    fn coordinate_to_index(&self, x: usize, y: usize) -> usize {
+        y * self.width + x
+    }
+
+    fn index_to_coordinate(&self, index: usize) -> (usize, usize) {
+        let x = index % self.width;
+        let y = (index - x) / self.width;
+        (x, y)
     }
 }
 
-fn is_neighbor(dx: usize, dy: usize) -> bool {
-    is_adjunct(dx) && is_adjunct(dy) && !same_field(dx, dy)
-}
-
-fn is_adjunct(offset: usize) -> bool {
-    offset == 0 || offset == 1
-}
-
-fn same_field(dx: usize, dy: usize) -> bool {
-    dx == 0 && dy == 0
+fn neighbor_offsets() -> impl Iterator<Item = (isize, isize)> {
+    NEIGHBOR_OFFSETS
+        .into_iter()
+        .flat_map(|i| [i].into_iter().cycle().zip(NEIGHBOR_OFFSETS))
+        .filter(|(x, y)| !(*x == 0 && *y == 0))
 }
