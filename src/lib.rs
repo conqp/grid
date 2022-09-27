63bd407fb388364bb802269e7c6773fabf7c4059
Richard Neumann <r.neumann@homeinfo.de>
2022-09-27 22:00:26 +0200

    Refactor

--- src/lib.rs
+++ src/lib.rs
@@ -29,16 +29,16 @@ impl<T> Grid<T> {
     }
 
     pub fn get(&self, x: usize, y: usize) -> Result<&T, &str> {
-        if on_grid(self.width, self.height, x as isize, y as isize) {
-            Ok(&self.items[coordinate_to_index(self.width, x, y)])
+        if self.on_grid(x as isize, y as isize) {
+            Ok(&self.items[self.coordinate_to_index(x, y)])
         } else {
             Err("coordinate not on grid")
         }
     }
 
     pub fn get_mut(&mut self, x: usize, y: usize) -> Result<&mut T, &str> {
-        if on_grid(self.width, self.height, x as isize, y as isize) {
-            let index = coordinate_to_index(self.width, x, y);
+        if self.on_grid(x as isize, y as isize) {
+            let index = self.coordinate_to_index(x, y);
             Ok(&mut self.items[index])
         } else {
             Err("coordinate not on grid")
@@ -55,54 +55,58 @@ impl<T> Grid<T> {
 
     pub fn enumerate(&self) -> impl Iterator<Item = (usize, usize, &T)> {
         self.items.iter().enumerate().map(|(index, item)| {
-            let (x, y) = index_to_coordinate(self.width, index);
+            let x = index % self.width;
+            let y = (index - x) / self.width;
             (x, y, item)
         })
     }
 
     pub fn enumerate_mut(&mut self) -> impl Iterator<Item = (usize, usize, &mut T)> {
         self.items.iter_mut().enumerate().map(|(index, item)| {
-            let (x, y) = index_to_coordinate(self.width, index);
+            let x = index % self.width;
+            let y = (index - x) / self.width;
             (x, y, item)
         })
     }
 
     pub fn neighbors(&self, x: usize, y: usize) -> impl Iterator<Item = (usize, usize, &T)> {
-        neighbor_coordinates(self.width, self.height, x, y)
-            .map(|(x, y)| (x, y, &self.items[coordinate_to_index(self.width, x, y)]))
+        let neigbors = self.neighbor_coordinates(x, y);
+        self.enumerate()
+            .filter(move |(x, y, _)| neigbors.iter().any(|(nx, ny)| *nx == *x && *ny == *y))
+    }
+
+    pub fn neighbors_mut(
+        &mut self,
+        x: usize,
+        y: usize,
+    ) -> impl Iterator<Item = (usize, usize, &mut T)> {
+        let neigbors: Vec<(usize, usize)> = self.neighbor_coordinates(x, y);
+        self.enumerate_mut()
+            .filter(move |(x, y, _)| neigbors.iter().any(|(nx, ny)| *nx == *x && *ny == *y))
+    }
+
+    fn neighbor_coordinates(&self, x: usize, y: usize) -> Vec<(usize, usize)> {
+        neighbor_offsets()
+            .map(move |(dx, dy)| (x as isize + dx, y as isize + dy))
+            .filter(move |&(x, y)| self.on_grid(x, y))
+            .map(|(dx, dy)| (dx as usize, dy as usize))
+            .collect()
+    }
+
+    fn on_grid(&self, x: isize, y: isize) -> bool {
+        0 <= x && x < self.width as isize && 0 <= y && y < self.height as isize
+    }
+
+    fn coordinate_to_index(&self, x: usize, y: usize) -> usize {
+        y * self.width + x
     }
 }
 
-fn coordinate_to_index(width: usize, x: usize, y: usize) -> usize {
-    y * width + x
-}
-
-fn index_to_coordinate(width: usize, index: usize) -> (usize, usize) {
-    let x = index % width;
-    let y = (index - x) / width;
-    (x, y)
-}
-
-fn neighbor_coordinates(
-    width: usize,
-    height: usize,
-    x: usize,
-    y: usize,
-) -> impl Iterator<Item = (usize, usize)> {
-    neighbor_offsets(2)
-        .map(move |item| (x as isize + item[0], y as isize + item[1]))
-        .filter(move |&(x, y)| on_grid(width, height, x, y))
-        .map(|(dx, dy)| (dx as usize, dy as usize))
-}
-
-fn on_grid(width: usize, height: usize, x: isize, y: isize) -> bool {
-    0 <= x && x < width as isize && 0 <= y && y < height as isize
-}
-
-fn neighbor_offsets(dimension: usize) -> impl Iterator<Item = Vec<isize>> {
+fn neighbor_offsets() -> impl Iterator<Item = (isize, isize)> {
     (-1..1)
         .map(|index| index as isize)
-        .combinations_with_replacement(dimension)
+        .combinations_with_replacement(2)
         // skip zero offset
         .filter(|items| !items.iter().all(|&item| item == 0))
+        .map(|items| (items[0], items[1]))
 }
