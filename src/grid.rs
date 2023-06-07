fa1cb40ff389efc40d21049d25f1d1ef8ebcfb9e
Richard Neumann <r.neumann@homeinfo.de>
2023-06-07 13:49:00 +0200

    Add test for TryFrom

--- src/grid.rs
+++ src/grid.rs
@@ -193,6 +193,18 @@ where
     }
 }
 
+/// Create a Grid from a tuple of an iterable and the desired width.
+///
+/// Examples:
+///
+/// ```
+/// use grid2d::{Grid, GridConstructionError};
+///
+/// let items = vec![1, 2, 3, 4, 5, 6, 7, 8];
+///
+/// assert!(Grid::try_from((items.clone(), 4)).is_ok());
+/// assert_eq!(Some(GridConstructionError::VecSizeNotMultipleOfWidth), Grid::try_from((items.clone(), 3)).err());
+/// assert_eq!(Some(GridConstructionError::ZeroSize), Grid::try_from((items.clone(), 0)).err());
 impl<T> TryFrom<(T, usize)> for Grid<T::Item>
 where
     T: IntoIterator,
