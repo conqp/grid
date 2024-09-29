d84836c25997211879d0af53ff2b6e12472d2b50
Richard Neumann <mail@richard-neumann.de>
2024-09-29 20:30:07 +0200

    Add crate docstring

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+//! A 2-dimensional grid with runtime-defined, fixed size that allows mutable access to its fields.
+
 pub use coordinate::Coordinate;
 pub use errors::{CoordinateParseError, GridConstructionError};
 pub use grid::Grid;
